59c0d22da054dbab93bd008f8c5e2cc9a5b6115e
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-04-14 18:15:11 +0330

    media file

--- 3-django-shop/A/A/settings.py
+++ 3-django-shop/A/A/settings.py
@@ -124,6 +124,10 @@ STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
