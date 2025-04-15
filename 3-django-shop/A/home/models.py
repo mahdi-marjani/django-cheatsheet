339dc037a7e0b309334e7b1804043237864213ec
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-04-15 21:30:29 +0330

    show products and product detail

--- 3-django-shop/A/home/models.py
+++ 3-django-shop/A/home/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -28,3 +29,6 @@ class Product(models.Model):
     
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('home:product_detail', args=[self.slug])
--- 3-django-shop/A/home/urls.py
+++ 3-django-shop/A/home/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
