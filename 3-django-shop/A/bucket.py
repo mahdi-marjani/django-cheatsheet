0d4292c99f097a02b6399387704e47365d08daf4
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-04-19 14:11:36 +0330

    Implement CDN bucket management and integrate with views and templates

---
+++ 3-django-shop/A/bucket.py
@@ -0,0 +1,39 @@
+import boto3
+from botocore.exceptions import ClientError
+from django.conf import settings # A.settings.py
+import logging
+
+class Bucket:
+    """
+    CDN bucket manager
+    
+    init method creates connection.
+    
+    NOTE:
+        none of these methods are async. use public interface in task.py module instead.
+    """
+    def __init__(self):
+        try:
+            self.s3_resource = boto3.resource(
+                service_name=settings.AWS_S3_SIGNATURE_VERSION,
+                aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
+                endpoint_url=settings.AWS_S3_ENDPOINT_URL
+            )
+        except Exception as exc:
+            logging.error(exc)
+    
+    def get_objects(self):
+        try:
+            bucket_name = settings.AWS_STORAGE_BUCKET_NAME
+            bucket = self.s3_resource.Bucket(bucket_name)
+            objects = bucket.objects.all()
+            if not objects:
+                logging.info(f"No objects found in bucket: {bucket_name}")
+                return None
+            return objects
+        except ClientError as e:
+            logging.error(e)
+
+
+bucket = Bucket()
