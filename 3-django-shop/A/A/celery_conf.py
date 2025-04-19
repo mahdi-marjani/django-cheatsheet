f13b2c90daec735a2346bfa59a6360e6b938ff11
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-04-19 12:00:55 +0330

    Add celery configuration and initialize celery app

---
+++ 3-django-shop/A/A/celery_conf.py
@@ -0,0 +1,19 @@
+from celery import Celery
+from datetime import timedelta
+import os
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'A.settings')
+
+celery_app = Celery('A')
+celery_app.autodiscover_tasks()
+
+celery_app.conf.update(
+    broker_url='amqp://admin:root@localhost:5672//',
+    result_backend='rpc://',
+    task_serializer='json',
+    result_serializer='pickle',
+    accept_content=['json', 'pickle'],
+    result_expires=timedelta(days=1),
+    task_always_eager=False,
+    worker_prefetch_multiplier=1,
+)
