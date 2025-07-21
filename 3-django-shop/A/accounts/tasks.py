01af5040787843d6f845c097de490503b0590123
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-21 12:46:09 +0330

    use celery beat

--- 3-django-shop/A/accounts/tasks.py
+++ 3-django-shop/A/accounts/tasks.py
@@ -1,6 +1,14 @@
 from celery import shared_task
 from utils import send_otp_code
+from accounts.models import OtpCode
+from django.utils import timezone
+from datetime import timedelta
 
 @shared_task
 def send_otp_code_task(phone, random_code):
     send_otp_code(phone, random_code)
+
+@shared_task
+def remove_expired_otps():
+    expired_time = timezone.now() - timedelta(minutes=2)
+    OtpCode.objects.filter(created__lt=expired_time).delete()
