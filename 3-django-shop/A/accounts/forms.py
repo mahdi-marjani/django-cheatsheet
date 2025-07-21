c74283017a7888dcbe685b34009677d5b80d148f
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-21 09:14:42 +0330

    clean otp code model

--- 3-django-shop/A/accounts/forms.py
+++ 3-django-shop/A/accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User, OtpCode
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 
@@ -51,6 +51,7 @@ class UserRegistrationForm(forms.Form):                         # for user regis
         user = User.objects.filter(phone_number=phone).exists()
         if user:
             raise ValidationError('Phone number is already taken')
+        OtpCode.objects.filter(phone_number=phone).delete()
         return phone
     
 class verifyCodeForm(forms.Form):
