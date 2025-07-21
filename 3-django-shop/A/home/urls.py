1b4b17af86c1d87b20f78ed07f80f68b1884ec65
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-21 18:14:30 +0330

    add category

--- 3-django-shop/A/home/urls.py
+++ 3-django-shop/A/home/urls.py
@@ -12,5 +12,6 @@ bucket_urls = [
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('bucket/', include(bucket_urls)),
+    path('category/<slug:category_slug>/', views.HomeView.as_view(), name='category_filter'),
     path('<slug:slug>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
--- 3-django-shop/A/home/views.py
+++ 3-django-shop/A/home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from .models import Product
+from .models import Product, Category
 from . import tasks
 from django.contrib import messages
 from .forms import UploadFileForm
@@ -9,9 +9,12 @@ import os
 from utils import IsAdminUserMixin
 
 class HomeView(View):
-    def get(self, request):
+    def get(self, request, category_slug=None):
         products = Product.objects.filter(available=True)
-        return render(request, 'home/home.html', {'products': products})
+        categories = Category.objects.all()
+        if category_slug:
+            products = products.filter(category__slug=category_slug)
+        return render(request, 'home/home.html', {'products': products, 'categories': categories})
 
 class ProductDetailView(View):
     def get(self, request, slug):
