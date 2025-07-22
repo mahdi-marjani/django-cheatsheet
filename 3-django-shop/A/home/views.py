e2311d3f7fac5d47d38c6add2f7b1708258749f0
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-22 19:19:41 +0330

    add cart detail

--- 3-django-shop/A/home/views.py
+++ 3-django-shop/A/home/views.py
@@ -7,6 +7,7 @@ from .forms import UploadFileForm
 from django.conf import settings # A.settings.py
 import os
 from utils import IsAdminUserMixin
+from orders.forms import CartAddForm
 
 class HomeView(View):
     def get(self, request, category_slug=None):
@@ -19,7 +20,8 @@ class HomeView(View):
 class ProductDetailView(View):
     def get(self, request, slug):
         product = get_object_or_404(Product, slug=slug)
-        return render(request, 'home/detail.html', {'product': product})
+        form = CartAddForm()
+        return render(request, 'home/detail.html', {'product': product, 'form': form})
 
 class BucketHome(IsAdminUserMixin, View):
     template_name = 'home/bucket.html'
