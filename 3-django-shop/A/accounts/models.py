cceea94c28b3f2ac3eba77c863cc4937196d135c
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-23 22:35:41 +0330

    add permissions

--- 3-django-shop/A/accounts/models.py
+++ 3-django-shop/A/accounts/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from .managers import UserManager
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     phone_number = models.CharField(max_length=11, unique=True)
     full_name = models.CharField(max_length=100)
@@ -17,11 +17,11 @@ class User(AbstractBaseUser):
     def __str__(self):
         return self.email
     
-    def has_perm(self, perm, obj=None):
-        return True
+    # def has_perm(self, perm, obj=None):
+    #     return True
     
-    def has_module_perms(self, app_label):
-        return True
+    # def has_module_perms(self, app_label):
+    #     return True
     
     @property
     def is_staff(self):
