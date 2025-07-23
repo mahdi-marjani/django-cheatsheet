ebb0cabcada4a91e71be40a9202ec43424912bd1
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-23 16:24:27 +0330

    use built-in validators

--- 3-django-shop/A/orders/models.py
+++ 3-django-shop/A/orders/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 from home.models import Product
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Order(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='orders')
     paid = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    discount = models.IntegerField(blank=True, null=True, default=None)
 
     class Meta:
         ordering = ('paid', '-updated')
@@ -15,7 +17,11 @@ class Order(models.Model):
         return f'{self.user} - {self.id}'
     
     def get_total_price(self):
-        return sum(item.get_cost() for item in self.items.all())
+        total = sum(item.get_cost() for item in self.items.all())
+        if self.discount:
+            discount_price = (self.discount / 100) * total
+            return int(total - discount_price)
+        return total
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
@@ -28,3 +34,13 @@ class OrderItem(models.Model):
     
     def get_cost(self):
         return self.price * self.quantity
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=30, unique=True)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(90)])
+    active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.code
--- 3-django-shop/A/orders/views.py
+++ 3-django-shop/A/orders/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
 from django.views import View
 from .cart import Cart
 from home.models import Product
-from .forms import CartAddForm
+from .forms import CartAddForm, CouponApplyForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import Order, OrderItem
+from .models import Order, OrderItem, Coupon
 import requests
 import json
+from django.utils import timezone
 from django.contrib import messages
 
 class CartView(View):
@@ -31,9 +32,11 @@ class CartRemoveView(View):
         return redirect('orders:cart')
 
 class OrderDetailView(LoginRequiredMixin, View):
+    form_class = CouponApplyForm
+
     def get(self, request, order_id):
         order = get_object_or_404(Order, id=order_id)
-        return render(request, 'orders/order.html', {'order': order})
+        return render(request, 'orders/order.html', {'order': order, 'form': self.form_class})
 
 class OrderCreateView(LoginRequiredMixin, View):
     def get(self, request):
@@ -117,3 +120,21 @@ class OrderVerifyView(LoginRequiredMixin, View):
                 data = {'status': False, 'code': str(response['data']['code'])}
                 return HttpResponse(f'data: {data}')
         return HttpResponse(response)
+    
+class CouponApplyView(LoginRequiredMixin, View):
+    form_class = CouponApplyForm
+    def post(self, request, order_id):
+        now = timezone.now()
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            code = form.cleaned_data['code']
+            try:
+                coupon = Coupon.objects.get(code__exact=code, valid_from__lte=now, valid_to__gte=now, active=True)
+            except Coupon.DoesNotExist:
+                messages.error(request, 'this coupon does not exist', 'danger')
+                return redirect('orders:order_detail', order_id)
+            
+            order = Order.objects.get(id=order_id)
+            order.discount = coupon.discount
+            order.save()
+        return redirect('orders:order_detail', order_id)
