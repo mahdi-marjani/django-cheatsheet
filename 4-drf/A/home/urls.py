347d994b397246841cef0052dfae4ef6fbd768b7
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-27 14:31:50 +0330

    create api

--- 4-drf/A/home/urls.py
+++ 4-drf/A/home/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', views.Home.as_view(), name='home')
+    path('', views.Home.as_view(), name='home')    # endpoint
 ]
--- 4-drf/A/home/views.py
+++ 4-drf/A/home/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.views import View
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-class Home(View):
+
+class Home(APIView):
     def get(self, request):
-        return render(request, 'home/home.html')
+        return Response({"message": "Hello, world!"})
