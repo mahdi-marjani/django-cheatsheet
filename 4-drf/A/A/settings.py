165c7530192cd8cb19db7e966a1ea308d8467aee
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-27 11:55:35 +0330

    initialize django rest framework

--- 4-drf/A/A/settings.py
+++ 4-drf/A/A/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Local apps
+    'home.apps.HomeConfig',
+
+    # Third-party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +60,9 @@ ROOT_URLCONF = 'A.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
