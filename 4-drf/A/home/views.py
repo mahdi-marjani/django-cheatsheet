80ef98fbf48e82de714ed494c147f3757325d864
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-28 08:48:33 +0330

    add request object

--- 4-drf/A/home/views.py
+++ 4-drf/A/home/views.py
@@ -4,4 +4,9 @@ from rest_framework.views import APIView
 
 class Home(APIView):
     def get(self, request):
-        return Response({"message": "Hello, world!"})
+        message = request.query_params['msg']
+        return Response({"message": message})
+
+    def post(self, request):
+        message = request.data['msg']
+        return Response({"message": message})
