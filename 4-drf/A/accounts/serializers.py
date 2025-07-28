a44991a384c6c281729eb25abd419ebb3319ee9b
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-28 12:08:53 +0330

    add custom serializer validator

--- 4-drf/A/accounts/serializers.py
+++ 4-drf/A/accounts/serializers.py
@@ -1,6 +1,21 @@
 from rest_framework import serializers
 
+def clean_email(value):
+    if 'admin' in value:
+        raise serializers.ValidationError("admin can't be in email")
+
 class UserRegisterSerializer(serializers.Serializer):
     username = serializers.CharField(required=True)
-    email = serializers.EmailField(required=True)
+    email = serializers.EmailField(required=True, validators=[clean_email])
     password = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
+
+    def validate_username(self, value):
+        if value == 'admin':
+            raise serializers.ValidationError("username can't be `admin`")
+        return value
+    
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("password must match")
+        return data
