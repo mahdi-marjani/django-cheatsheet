a4d551e352ccd3244c16f3f7e0469b9f686e86d5
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-29 22:51:00 +0330

    clean question view & method fields

--- 4-drf/A/home/serializers.py
+++ 4-drf/A/home/serializers.py
@@ -8,9 +8,15 @@ class PersonSerializer(serializers.Serializer):
     email = serializers.EmailField()
 
 class QuestionSerializer(serializers.ModelSerializer):
+    answers = serializers.SerializerMethodField()
+
     class Meta:
         model = Question
         fields = '__all__'
+    
+    def get_answers(self, obj):
+        result = obj.answers.all()
+        return AnswerSerializer(instance=result, many=True).data
 
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
--- 4-drf/A/home/views.py
+++ 4-drf/A/home/views.py
@@ -14,12 +14,13 @@ class Home(APIView):
         ser_data = PersonSerializer(instance=persons, many=True)
         return Response({"data": ser_data.data})
 
-class QuestionView(APIView):
+class QuestionListView(APIView):
     def get(self, request):
         questions = Question.objects.all()
         srz_data = QuestionSerializer(instance=questions, many=True)
         return Response(srz_data.data, status=status.HTTP_200_OK)
 
+class QuestionCreateView(APIView):
     def post(self, request):
         srz_data = QuestionSerializer(data=request.data)
         if srz_data.is_valid():
@@ -27,6 +28,7 @@ class QuestionView(APIView):
             return Response(srz_data.data, status=status.HTTP_201_CREATED)
         return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class QuestionUpdateView(APIView):
     def put(self, request, pk):
         question = Question.objects.get(pk=pk)
         srz_data = QuestionSerializer(
@@ -39,6 +41,7 @@ class QuestionView(APIView):
             return Response(srz_data.data, status=status.HTTP_200_OK)
         return Response(srz_data.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class QuestionDeleteView(APIView):
     def delete(self, request, pk):
         question = Question.objects.get(pk=pk)
         question.delete()
