814dcb794771169be5448f47509aa7b18e867f76
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-29 14:43:35 +0330

    use permissions

--- 4-drf/A/home/views.py
+++ 4-drf/A/home/views.py
@@ -2,9 +2,12 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Person
 from .serializers import PersonSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class Home(APIView):
+    permission_classes = [IsAdminUser,]
+
     def get(self, request):
         persons = Person.objects.all()
         ser_data = PersonSerializer(instance=persons, many=True)
