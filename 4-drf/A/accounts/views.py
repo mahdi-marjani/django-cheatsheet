526490d46b9ef8d8b9e20d5df863de9e74f20877
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-29 11:33:11 +0330

    use status codes

--- 4-drf/A/accounts/views.py
+++ 4-drf/A/accounts/views.py
@@ -1,12 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from django.contrib.auth.models import User
 from .serializers import UserRegisterSerializer
+from rest_framework import status
 
 class UserRegister(APIView):
     def post(self, request):
         ser_data = UserRegisterSerializer(data=request.data)
         if ser_data.is_valid():
             ser_data.create(ser_data.validated_data)
-            return Response(ser_data.data)
-        return Response(ser_data.errors)
+            return Response(ser_data.data, status=status.HTTP_201_CREATED)
+        return Response(ser_data.errors, status=status.HTTP_400_BAD_REQUEST)
