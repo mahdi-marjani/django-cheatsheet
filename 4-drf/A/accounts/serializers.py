18325fea50c9f1e9adaa2b7904c2e84a4bc36bad
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-30 16:20:27 +0330

    use viewset

--- 4-drf/A/accounts/serializers.py
+++ 4-drf/A/accounts/serializers.py
@@ -32,3 +32,8 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         if data['password'] != data['password2']:
             raise serializers.ValidationError("password must match")
         return data
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
--- 4-drf/A/accounts/urls.py
+++ 4-drf/A/accounts/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
 from rest_framework.authtoken import views as authtoken_views
+from rest_framework import routers
 
 app_name = 'accounts'
 urlpatterns = [
     path('register/', views.UserRegister.as_view()),
     path('api-token-auth/', authtoken_views.obtain_auth_token),
 ]
+
+router = routers.SimpleRouter()
+router.register(r'user', views.UserViewSet)
+urlpatterns += router.urls
