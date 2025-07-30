ffecc378eaf067cd8e893dc2ee2dec5e76688d51
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-07-30 10:28:15 +0330

    update A/home/urls.py

--- 4-drf/A/home/urls.py
+++ 4-drf/A/home/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),    # endpoint
-    path('questions/', views.QuestionListView.as_view()),           # GET
-    path('questions/', views.QuestionCreateView.as_view()),           # POST
-    path('questions/<int:pk>', views.QuestionUpdateView.as_view()),    # PUT
-    path('questions/<int:pk>', views.QuestionDeleteView.as_view())    # DELETE
+    path('questions/', views.QuestionListView.as_view()),                     # GET
+    path('question/create/', views.QuestionCreateView.as_view()),             # POST
+    path('question/update/<int:pk>/', views.QuestionUpdateView.as_view()),    # PUT
+    path('question/delete/<int:pk>/', views.QuestionDeleteView.as_view())     # DELETE
 ]
