092b835a6306e9bb1265801a9289e46db872cb68
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-08-02 15:01:07 +0330

    use RedirectView

--- 5-django-cbv/A/home/urls.py
+++ 5-django-cbv/A/home/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
+    path('two/<int:id>/<str:name>/', views.Two.as_view(), name='two'),
 ]
--- 5-django-cbv/A/home/views.py
+++ 5-django-cbv/A/home/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from .models import Car
 
 
@@ -9,3 +9,18 @@ class Home(TemplateView):
         context = super().get_context_data(**kwargs)
         context['cars'] = Car.objects.all()
         return context
+
+class Two(RedirectView):
+    # url = 'https://google.com'
+    # pattern_name = 'home:home'
+    # url = '/'
+    url = '/home/%(id)i/%(name)s'
+    query_string = True
+
+    def get_redirect_url(self, *args, **kwargs):
+        # print('processing your request...')
+        # print(kwargs['id'])
+        # print(kwargs['name'])
+        # kwargs.pop('id')
+        # kwargs.pop('name')
+        return super().get_redirect_url(*args, **kwargs)
