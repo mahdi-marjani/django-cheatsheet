9b2f7198d55e991cd1a53c3cc030bb6350ca561f
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-08-02 13:53:59 +0330

    add View

--- 5-django-cbv/A/home/views.py
+++ 5-django-cbv/A/home/views.py
@@ -3,5 +3,17 @@ from django.views import View
 
 
 class Home(View):
-    def get(self, request):
-        return render(request, 'home/home.html')
+    http_method_names = ['post', 'options']
+
+    def post(self, request):
+        ...
+    
+    def options(self, request, *args, **kwargs):
+        response = super().options(request, *args, **kwargs)
+        response.headers['host'] = 'localhost'
+        response.headers['user'] = request.user
+        return response
+    
+    def http_method_not_allowed(self, request, *args, **kwargs):
+        super().http_method_not_allowed(request, *args, **kwargs)
+        return render(request, 'method_not_allowed.html')
