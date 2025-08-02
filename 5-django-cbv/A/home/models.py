178446bcf43b44823b18d49794c39cccc4d2ee02
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-08-02 14:19:10 +0330

    use TemplateView

--- 5-django-cbv/A/home/models.py
+++ 5-django-cbv/A/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Car(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.CharField(max_length=100)
+    year = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return self.name
+    
--- 5-django-cbv/A/home/views.py
+++ 5-django-cbv/A/home/views.py
@@ -1,19 +1,11 @@
-from django.shortcuts import render
-from django.views import View
+from django.views.generic import TemplateView
+from .models import Car
 
 
-class Home(View):
-    http_method_names = ['post', 'options']
+class Home(TemplateView):
+    template_name = 'home/home.html'
 
-    def post(self, request):
-        ...
-    
-    def options(self, request, *args, **kwargs):
-        response = super().options(request, *args, **kwargs)
-        response.headers['host'] = 'localhost'
-        response.headers['user'] = request.user
-        return response
-    
-    def http_method_not_allowed(self, request, *args, **kwargs):
-        super().http_method_not_allowed(request, *args, **kwargs)
-        return render(request, 'method_not_allowed.html')
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['cars'] = Car.objects.all()
+        return context
