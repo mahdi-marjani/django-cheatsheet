f50909d49edc8d85b5d5de84b3c5b48b1d31f96c
mahdi-marjani <mahdi.marjani.dev@gmail.com>
2025-08-03 10:53:57 +0330

    use DeleteView

--- 5-django-cbv/A/home/urls.py
+++ 5-django-cbv/A/home/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.Home.as_view(), name='home'),
-    path('create/', views.CreateCarView.as_view(), name='car_create'),
+    path('delete/<int:pk>/', views.CarDelete.as_view(), name='car_delete'),
 ]
--- 5-django-cbv/A/home/views.py
+++ 5-django-cbv/A/home/views.py
@@ -1,30 +1,13 @@
 from .models import Car
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, DeleteView
 from django.urls import reverse_lazy
-from django.contrib import messages
 
 class Home(ListView):
     template_name = 'home/home.html'
     model = Car
     context_object_name = 'cars'
 
-class CreateCarView(CreateView):
+class CarDelete(DeleteView):
     model = Car
-    fields = ['name', 'year']
-    template_name = 'home/create.html'
     success_url = reverse_lazy('home:home')
-
-    def form_valid(self, form):
-        car = form.save(commit=False)
-        user = self.request.user.username
-        if user:
-            car.owner = user
-        else:
-            car.owner = 'anonymous'
-        car.save()
-        messages.success(
-            self.request,
-            'created car successfully',
-            'success'
-        )
-        return super().form_valid(form)
+    template_name = 'home/delete.html'
